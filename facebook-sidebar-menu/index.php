8e31629afae2685a5762c0cedc57a348446c4bce
MartinKrestan <martin_krestan@hotmail.com>
2013-07-18 12:21:48 +1000

    1.0 - Files Added

---
+++ facebook-sidebar-menu/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<meta name="description" content="Create a Facebook style mobile sidebar menu using only CSS and minimal JavaScript"/>
+	<meta name="keywords" content="facebook menu,javascript,mobile side menu, jquery"/>
+	<meta name="robots" content="all"/>
+	<meta name="author" content="Martin Krestan" />
+	<title>Create a Facebook style mobile sidebar menu using only CSS and minimal JavaScript - No need for jQuery Mobile, Twitter Bootstrap or anything like that | Martin Krestan</title>
+	<!-- START Javascript -->
+	<script type="text/javascript" src="/js/jquery-1.7.min.js"></script>
+	<script type="text/javascript" src="js/slide-menu.js"></script>
+	<!-- START CSS -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<!-- Mobile Header START -->
+<div class="mobile-header">
+	<ul class="clearfloat">
+		<li class="m-menu">
+			<a><span class="menu-open">Menu</span><span class="menu-close">Close</span></a>
+		</li>
+		<li>
+			<a href="http://www.mywork.com.au/blog/how-to-create-a-simple-bacebook-style-sidebar-menu-using-only-css-and-minimal-javascript-no-need-for-jquery-mobile-or-twitter-bootstrap/">Back To Blog Post</a>
+		</li>
+		<li>
+			<a href="http://www.mywork.com.au/blog/author/martin/">More Posts By Martin</a>
+		</li>
+		<li>
+			<a href="http://www.mywork.com.au">MyWork.com.au</a>
+		</li>
+	</ul>
+</div>
+<!-- Mobile Header END -->
+
+<div id="container-outer">
+
+	<!-- Mobile Menu START -->
+	<div class="mobile-menu">
+		Menu content to go here
+	</div>
+	<!-- Mobile Menu END -->
+	
+	<div id="container">
+		<div id="container-inner">
+			<h1>Create a Facebook style sidebar menu using only CSS and minimal JavaScript</h1>
+			<h2>No need for jQuery Mobile</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget enim posuere mi cursus hendrerit. Integer id mauris ipsum. In elementum sapien eget sem porttitor eu ultricies mauris auctor.</p>
+			<p>Vivamus vel tortor non nisi pellentesque molestie. Aenean consectetur tristique vehicula. Integer congue ante id diam tristique aliquet. Maecenas dictum, nibh eget pretium pulvinar, mi justo aliquet mauris, ut ultrices augue nisi nec sapien. Sed quis lobortis libero. In magna libero, molestie non malesuada interdum, dignissim quis dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed malesuada ante quis enim iaculis auctor. Vestibulum venenatis, nisl sagittis eleifend, augue ceehati enim congue arcu, non laoreet augue felis vel risus. Etiam ut sapien sed massa egestas convallis vitae ac metus. Aenean ut ligula sit amet nibh posuere venenatis eu non nisl.</p>
+			<p>Donec aliquet, nisi ac lobortis iaculis, neque leo convallis ante, ac viverra turpis orci id nisl. Curabitur placerat laoreet placerat. Morbi at sapien eget ipsum dapibus fermentum. Phasellus quis leo sed metus faucibus placerat. Praesent feugiat tincidunt ligula, at imperdiet ligula porta quis. Sed vel accumsan libero. Aliquam condimentum pretium turpis, fringilla sodales libero mollis eu. Nullam pellentesque, eros ceehati euismod commodo consectetur, ante orci euismod sem, vel volutpat tellus diam eget tortor.</p>
+			<p>Curabitur ut purus in dui dapibus consectetur. Morbi sit amet justo ligula, eu porttitor tellus. Morbi dignissim venenatis dignissim. Aenean nisi enim, aliquam nec faucibus at, egestas nec tellus. Donec in neque eu leo scelerisque sodales a id ante. Suspendisse ligula dui, ultrices sit amet rutrum ut, dapibus ut lacus. Nullam aliquam egestas feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus hendrerit egestas lacinia. Duis id magna ultrices lectus malesuada ullamcorper. Integer ultricies iaculis lorem sollicitudin hendrerit. Nullam non ante eget libero varius ullamcorper.</p>
+			<p>In sed metus massa. Nulla arcu mi, suscipit ac mattis sed, ornare ut sapien. Mauris ut leo ligula. Nunc quis tortor vel elit luctus aliquet. Pellentesque placerat est vel urna malesuada at aliquam erat lacinia. Duis porta cursus leo, ac sodales nunc adipiscing eu. Nulla facilisi.</p>
+		</div>
+	</div>
+
+	<div class="mobile-menu-bg"></div>
+</div><!-- Full Site Container Outer End -->
+</body>
+</html>
---
+++ facebook-sidebar-menu/js/slide-menu.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$(".m-menu").click(function(){
+	
+		$("#container").toggleClass("active");
+		$(".m-menu").toggleClass("active");
+		$(".mobile-menu").toggleClass("active");
+		$(".mobile-menu-bg").addClass("active");
+	});
+	
+	$(".mobile-menu").css('left','-200px');
+	$(".mobile-menu-bg").css('left','-200px');
+	$(".m-menu").toggle(function() {
+		$('.mobile-menu').animate({ left: '0' }, { duration: 500, queue: false });
+		$('.mobile-menu-bg').animate({ left: '0' }, { duration: 500, queue: false });
+		$('#container').animate({ 'margin-left': '200px' }, { duration: 500, queue: false });
+		$('#container').animate({ 'margin-right': '-200px' }, { duration: 500, queue: false });
+	}, function() {       
+		$('.mobile-menu-bg').animate({ left: '-200' }, { duration: 500, queue: false });
+		$('.mobile-menu').animate({ left: '-200' }, { duration: 500, queue: false });
+		$('#container').animate({ 'margin-left': '0' }, { duration: 500, queue: false });
+		$('#container').animate({ 'margin-right': '0' }, { duration: 500, queue: false });
+	}
+	);
+})
