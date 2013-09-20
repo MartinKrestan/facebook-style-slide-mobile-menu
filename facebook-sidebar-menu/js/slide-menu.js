7c5981a46825be2f2847078536faa15febe867e6
MartinKrestan <martin_krestan@hotmail.com>
2013-09-20 10:06:02 +1000

    jQuery Compatibility Update
    The new code strips the toggle function and replaces it with a subscription to the click event.  This will now make it compatible with jQuery 1.90+.
    
    Big thanks to my mate Chris for the fix. https://github.com/cpmcgrath

--- facebook-sidebar-menu/js/slide-menu.js
+++ facebook-sidebar-menu/js/slide-menu.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$(".m-menu").click(function(){
-	
 		$("#container").toggleClass("active");
 		$(".m-menu").toggleClass("active");
 		$(".mobile-menu").toggleClass("active");
@@ -8,17 +7,22 @@ $(document).ready(function() {
 	});
 	
 	$(".mobile-menu").css('left','-200px');
-	$(".mobile-menu-bg").css('left','-200px');
-	$(".m-menu").toggle(function() {
-		$('.mobile-menu').animate({ left: '0' }, { duration: 500, queue: false });
-		$('.mobile-menu-bg').animate({ left: '0' }, { duration: 500, queue: false });
-		$('#container').animate({ 'margin-left': '200px' }, { duration: 500, queue: false });
-		$('#container').animate({ 'margin-right': '-200px' }, { duration: 500, queue: false });
-	}, function() {       
-		$('.mobile-menu-bg').animate({ left: '-200' }, { duration: 500, queue: false });
-		$('.mobile-menu').animate({ left: '-200' }, { duration: 500, queue: false });
-		$('#container').animate({ 'margin-left': '0' }, { duration: 500, queue: false });
-		$('#container').animate({ 'margin-right': '0' }, { duration: 500, queue: false });
-	}
-	);
+	$(".mobile-menu-bg").css('left', '-200px');
+
+	var isExpanded = false;
+	$(".m-menu").click(function () {
+	    if (!isExpanded) {
+	        $('.mobile-menu').animate({ left: '0' }, { duration: 500, queue: false });
+	        $('.mobile-menu-bg').animate({ left: '0' }, { duration: 500, queue: false });
+	        $('#container').animate({ 'margin-left': '200px' }, { duration: 500, queue: false });
+	        $('#container').animate({ 'margin-right': '-200px' }, { duration: 500, queue: false });
+	    }
+	    else {
+	        $('.mobile-menu-bg').animate({ left: '-200' }, { duration: 500, queue: false });
+	        $('.mobile-menu').animate({ left: '-200' }, { duration: 500, queue: false });
+	        $('#container').animate({ 'margin-left': '0' }, { duration: 500, queue: false });
+	        $('#container').animate({ 'margin-right': '0' }, { duration: 500, queue: false });
+	    }
+	    isExpanded = !isExpanded;
+	});
 })
